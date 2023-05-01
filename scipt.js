e1f2fad1b12731271226dc1225eae9e4fc3447f0
juanmarcosls <juanmarcoslemossmit@gmail.com>
2023-05-01 11:53:10 -0300

    Dando funcionalidad a los botones

--- scipt.js
+++ scipt.js
@@ -1,3 +1,45 @@
+// Boton Like
+
+function btnLike(){
+  var count = 200;
+  var counter = document.getElementById("counter");
+  var incrementBtn = document.getElementById("btnLike");
+
+  
+  if(incrementBtn.textContent === "Like"){
+      incrementBtn.textContent = "Dislike";
+      incrementBtn.style.backgroundColor = "white";
+      incrementBtn.style.color = "black";
+      count++;
+      counter.innerText = count; 
+  }
+  else {
+    count = 201
+    incrementBtn.textContent = "Like";
+    incrementBtn.style.backgroundColor = "black";
+    incrementBtn.style.color = "white";
+    count--;
+    counter.innerText = count; 
+  }
+}
+
+// Boton Seguir - Dejar de Seguir
+
+function cambiarBotonFollow() {
+var btnFollow = document.getElementById("btnFollow");
+  
+  if (btnFollow.textContent === "Seguir") {
+    btnFollow.style.backgroundColor = "white";
+    btnFollow.style.color = "black";
+    btnFollow.textContent = "Dejar de seguir ";
+  } else {
+    btnFollow.style.backgroundColor = "black";
+    btnFollow.style.color = "white";
+    btnFollow.textContent = "Seguir";
+   }
+}
+
+
 function agregarTarea(evento) {
     evento.preventDefault();
 
@@ -18,3 +60,5 @@ function agregarTarea(evento) {
 }
 
 document.getElementById('boton-agregar-tarea').addEventListener('click', agregarTarea);
+
+
